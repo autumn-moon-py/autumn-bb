6ac09029a349611e01e3ed0a562168c748f2a2fa
autumn-moon-py <1024120029@qq.com>
2026-07-25 20:49:46 +0800

    update: 2 files changed (2026-07-25 20:49)

---
+++ git_sync.bat
@@ -0,0 +1,175 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Multi-repo Git sync tool for Windows
+:: Place in parent directory of repos, double-click to run
+
+set "SD=%~dp0"
+set "FL="
+
+echo ============================================
+echo   Git Multi-Repo Sync Tool
+echo ============================================
+echo.
+
+:: Scan repos
+set "RC=0"
+for /d %%D in ("%SD%*") do (
+    if exist "%%D\.git" (
+        set /a RC+=1
+        set "R!RC!=%%D"
+        echo   [!RC!] %%~nxD
+    )
+)
+
+if %RC%==0 (
+    echo No Git repos found.
+    pause
+    exit /b 1
+)
+
+echo.
+echo Found %RC% repo(s).
+echo.
+
+:: --- Fetch + Rebase ---
+echo --------------------------------------------
+echo   [1/3] Fetch + Rebase
+echo --------------------------------------------
+set /p "C=Start fetch? (Y/n): "
+if /i "!C!"=="n" goto :commit_phase
+
+set "I=0"
+:fetch
+set /a I+=1
+if !I! gtr %RC% goto :commit_phase
+set "P=!R%I%!"
+for %%N in ("!P!") do set "N=%%~nxN"
+echo [!N!]
+pushd "!P!"
+git fetch --all 2>&1
+if !errorlevel! equ 0 goto :rebase
+echo   [FAIL] fetch
+set "FL=!FL! !N!"
+popd
+echo.
+goto :fetch
+:rebase
+git rebase 2>&1
+if !errorlevel! equ 0 goto :fetch_ok
+echo   [CONFLICT] rebase skipped
+git rebase --abort >nul 2>&1
+set "FL=!FL! !N!"
+popd
+echo.
+goto :fetch
+:fetch_ok
+echo   [OK]
+popd
+echo.
+goto :fetch
+
+:: --- Commit ---
+:commit_phase
+echo.
+echo --------------------------------------------
+echo   [2/3] Commit
+echo --------------------------------------------
+
+set "HC=0"
+set "I=0"
+:check
+set /a I+=1
+if !I! gtr %RC% goto :check_done
+set "P=!R%I%!"
+set "CC=0"
+pushd "!P!"
+for /f %%C in ('git status --porcelain 2^>nul ^| find /c /v ""') do set "CC=%%C"
+popd
+if not !CC! gtr 0 goto :check_next
+for %%N in ("!P!") do set "N=%%~nxN"
+echo   !N!: !CC! file(s) changed
+set "HC=1"
+:check_next
+goto :check
+
+:check_done
+if !HC!==0 (
+    echo No changes to commit.
+    goto :push_phase
+)
+
+echo.
+set /p "C=Commit changes? (Y/n): "
+if /i "!C!"=="n" goto :push_phase
+
+set "I=0"
+:commit
+set /a I+=1
+if !I! gtr %RC% goto :push_phase
+set "P=!R%I%!"
+set "CC=0"
+pushd "!P!"
+for /f %%C in ('git status --porcelain 2^>nul ^| find /c /v ""') do set "CC=%%C"
+if not !CC! gtr 0 goto :commit_next
+for %%N in ("!P!") do set "N=%%~nxN"
+for /f "tokens=1-3 delims=/ " %%a in ("%date:~0,10%") do set "TD=%%a-%%b-%%c"
+set "TM=%time:~0,5%"
+set "MSG=update: !CC! files changed (!TD! !TM!)"
+git add -A
+git commit -m "!MSG!" 2>&1
+if !errorlevel! equ 0 (
+    echo   [!N!] committed: !MSG!
+) else (
+    echo   [!N!] commit failed
+    set "FL=!FL! !N!"
+)
+:commit_next
+popd
+goto :commit
+
+:: --- Push ---
+:push_phase
+echo.
+echo --------------------------------------------
+echo   [3/3] Push
+echo --------------------------------------------
+set /p "C=Push to remote? (Y/n): "
+if /i "!C!"=="n" goto :done
+
+set "I=0"
+:push
+set /a I+=1
+if !I! gtr %RC% goto :done
+set "P=!R%I%!"
+for %%N in ("!P!") do set "N=%%~nxN"
+set "U=0"
+pushd "!P!"
+for /f %%C in ('git log --oneline "@{u}..HEAD" 2^>nul ^| find /c /v ""') do set "U=%%C"
+if not !U! gtr 0 goto :push_skip
+echo [!N!] pushing !U! commit(s)...
+git push 2>&1
+if !errorlevel! neq 0 (
+    echo   [FAIL] push
+    set "FL=!FL! !N!"
+) else (
+    echo   [OK]
+)
+goto :push_next
+:push_skip
+echo [!N!] up to date
+:push_next
+popd
+goto :push
+
+:done
+echo.
+echo ============================================
+if defined FL (
+    echo   Failed:!FL!
+) else (
+    echo   All done.
+)
+echo ============================================
+echo.
+pause
